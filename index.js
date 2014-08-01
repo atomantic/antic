4fdefe6f96eead9b284fec79d70b258c1515706f
Adam Eivy <adam.eivy@disney.com>
2014-08-01 15:50:35 -0700

    now using comma-first because it's funny -- and adding my joke about the blue and red balls

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 var random = require('lodash.random');
 
 var jokes = [
-	"Atom walks into a bar. The bartender tells him to split. Everyone dies.",
-	"A mouse and an elephant are walking across a bridge. The mouse turns to the elephant and says, \"We make a lot of noise.\""
+	"Atom walks into a bar. The bartender tells him to split. Everyone dies."
+	,"A mouse and an elephant are walking across a bridge. The mouse turns to the elephant and says, \"We make a lot of noise.\""
+	,"A red ball says to a blue ball, \"tomorrow, I'm gonna be yellow.\" the blue ball just sits in silence because it's an inanimate object with no mouth."
 ];
 
 console.log(jokes[random(0, jokes.length-1)]);
