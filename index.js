8959bc1297388e4018d4975ec517d716df65dc0c
Adam Eivy <adam.eivy@disney.com>
2014-08-04 14:41:16 -0700

    once more with feeling

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
-var random = require('lodash.random');
+var random = require("lodash.random");
 
 var jokes = [
 	"Atom walks into a bar. The bartender tells him to split. Everyone dies."
 	,"A mouse and an elephant are walking across a bridge. The mouse turns to the elephant and says, \"We make a lot of noise.\""
 	,"A red ball says to a blue ball, \"tomorrow, I'm gonna be yellow.\" the blue ball just sits in silence because it's an inanimate object with no mouth."
+	,"git commit -m 'once more with feeling'"
+
 ];
 
-console.log(jokes[random(0, jokes.length-1)]);
+console.log("\n"+jokes[random(0, jokes.length-1)]);
