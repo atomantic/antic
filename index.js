101fb78e47de662972fca1d179d2dfce7858c3a2
Adam Eivy <adam.eivy@disney.com>
2014-08-20 14:12:09 -0700

    once more with feeling

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var jokes = [
 	,'A mouse and an elephant are walking across a bridge. The mouse turns to the elephant and says, "We make a lot of noise."'
 	,'A red ball says to a blue ball, "I can bounce higher than you!" The blue ball just sits in silence because it\'s an inanimate object with no mouth.'
 	,'git commit -m "once more with feeling"'
-	,'We\'ve started calling the bathroom "the gym". Now we can wake up every morning and say we went to the gym. Man, I spent a long time at the gym this morning.'
+	,'Man, I spent a long time at The Gym this morning. We\'ve started calling the bathroom "The Gym".'
 	,'I wish I could love unicorns, but love isn\'t real.'
 	,'Got arrested at the airport. Apparently, security doesn\'t appreciate it when you call "shotgun" before boarding a plane.'
 	,'Hammer walks into a bar. Bartender asks him how work went today. "Nailed it!"'
