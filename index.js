9638213a4b4f8b331a7f4548a3d2330ff81ee23d
Adam Eivy <atomantic@gmail.com>
2019-08-23 17:06:23 -0700

    remove lodash.sample dependency

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
-var sample = require('lodash.sample')
+var sample = function(arr){
+    var len = arr == null ? 0 : arr.length
+    return len ? arr[Math.floor(Math.random() * len)] : undefined
+}
 var jokes = require('./src/jokes')
 var lulz = require('./src/lulz')
 
